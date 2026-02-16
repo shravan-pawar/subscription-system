0d848cc2743259598b85c94e622cec15deb5091b
shravan-pawar <shravanpawar434@gmail.com>
2026-02-16 22:35:33 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,7 +1,23 @@
 <?php
-$conn = pg_connect("host=localhost dbname=subscription_system user=postgres password=psql");
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$host = getenv("DB_HOST");
+$port = getenv("DB_PORT");
+$dbname = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+
+$conn = pg_connect(
+    "host=$host 
+     port=$port 
+     dbname=$dbname 
+     user=$user 
+     password=$pass 
+     sslmode=require"
+);
 
 if (!$conn) {
-    die("Connection failed");
+    die("Database connection failed.");
 }
 ?>
